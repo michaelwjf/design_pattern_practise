6a483542aa8858f5aa6d5274861bf63b9b07aac5
jfw <junfeng.wu@live.cn>
2023-06-29 18:26:20 +0800

    added comments of Singleton02

--- singnleton/src/main/java/com/jfw/designpattern/singleton/Singleton02.java
+++ singnleton/src/main/java/com/jfw/designpattern/singleton/Singleton02.java
@@ -1,16 +1,23 @@
 package com.jfw.designpattern.singleton;
 
 /**
+ * 单例模式，懒汉式，线程不安全写法。这种写法是有问题的，生产上不能用这种线程不安全的写法
+ * <p>
+ * 优点： 起到了lazy loading的效果，但是只能在单线程下使用。
+ * <p>
+ * 缺点： 在多线程下，一个线程进入了if(instance == null)判断语句，还未来得及往下执行，
+ * 另一个线程也通过了这个判断语句，这时就会产生多个实例。所以在多线程环境下不可以使用这种方式。
+ *
  * @author jfw
  * @date 2021-07-16
- * <p>
- * 单例模式，懒汉式，线程不安全写法。这种写法是有问题的，生产上不能用这种线程不安全的写法
  */
 public class Singleton02 {
     /**
      * 先私有化构造器，避免new
      */
-    private Singleton02(){}
+    private Singleton02() {
+
+    }
 
     /**
      * 私有静态实例
