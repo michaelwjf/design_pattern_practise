df151dac2ea4f73e3a14993a23af7c8e503e51e7
Michael <michaelwjf@sina.com>
2023-07-05 19:19:23 +0800

    added SimpleFactory pattern

---
+++ factories/src/main/java/com/jfw/designpattern/factories/simplefactory/OrderPizza.java
@@ -0,0 +1,59 @@
+package com.jfw.designpattern.factories.simplefactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import com.jfw.designpattern.factories.withoutpattern.Pizza;
+
+/**
+ * 使用聚合的方式，所以从外面传入一个simpleFactory对象，
+ * 然后调用其createPizza方法根据输入的不同pizza类别创建不同的pizza。
+ * 当然也可以用组合的方式，即在内部创建一个simpleFactory属性，
+ * 总之就是要通过一个simpleFactory来创建pizza
+ *
+ * @author jfw
+ * @date 2023-07-05
+ */
+public class OrderPizza {
+    public OrderPizza(SimpleFactory simpleFactory) {
+        setFactory(simpleFactory);
+    }
+
+    private void setFactory(SimpleFactory factory) {
+        if (factory == null) {
+            System.out.println("need a simple factory object");
+            return;
+        }
+        String orderType = "";
+        Pizza pizza = null;
+
+        do {
+            orderType = getType();
+            pizza = factory.createPizza(orderType);
+            if (pizza != null) {
+                // pizza对象不为空表示订购pizza成功
+                pizza.prepare();
+                pizza.bake();
+                pizza.cut();
+                pizza.box();
+            } else {
+                System.out.println("订购pizza失败");
+                break;
+            }
+        } while (true);
+
+    }
+
+    private String getType() {
+        try {
+            BufferedReader bufferedReader =
+                    new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("input the pizza type: ");
+            return bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+}
--- factories/src/main/java/com/jfw/designpattern/factories/simplefactory/SimpleFactory.java
+++ factories/src/main/java/com/jfw/designpattern/factories/simplefactory/SimpleFactory.java
@@ -1,9 +1,34 @@
 package com.jfw.designpattern.factories.simplefactory;
 
+import com.jfw.designpattern.factories.withoutpattern.CheesePizza;
+import com.jfw.designpattern.factories.withoutpattern.GreekPizza;
+import com.jfw.designpattern.factories.withoutpattern.PepperPizza;
+import com.jfw.designpattern.factories.withoutpattern.Pizza;
+
 /**
+ * 创建一个简单工厂类，将所有订购不同种类pizza相关的操作都在这个类里面实现
  *
  * @author jfw
  * @date 2023-07-04
  */
 public class SimpleFactory {
+
+    /**
+     * 根据不同的orderType, 创建不同种类的pizza
+     * <p>
+     * 因为Pizza类和不同种类的Pizza之间的关系不变，
+     * 所以还是利用withoutpattern里面的各个Pizza类
+     *
+     * @param orderType String pizza type, e.g. greek, cheese, pepper
+     * @return pizza
+     */
+    public Pizza createPizza(String orderType) {
+        System.out.println(" 使用简单工厂模式 ");
+        return switch (orderType) {
+            case "greek" -> new GreekPizza();
+            case "cheese" -> new CheesePizza();
+            case "pepper" -> new PepperPizza();
+            default -> null;
+        };
+    }
 }
